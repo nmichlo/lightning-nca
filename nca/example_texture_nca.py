6c4cbdb3177518d82d111e54098f8cc63990f599
Nathan Michlo <NathanJMichlo@gmail.com>
2021-06-11 20:39:44 +0200

    Fire command line

--- nca/example_texture_nca.py
+++ nca/example_texture_nca.py
@@ -2,6 +2,7 @@
 # https://colab.research.google.com/github/google-research/self-organising-systems/blob/master/notebooks/texture_nca_pytorch.ipynb
 
 from datetime import datetime
+from functools import wraps
 from typing import Iterator
 
 import numpy as np
@@ -33,21 +34,24 @@ from nca.util.pl_callbacks import CallbackVidsaveNCA
 
 class TextureNCA(BaseNCA):
 
-    def __init__(self, channels=12, hidden_channels=96, learn_filters=False):
+    def __init__(self, channels=12, hidden_channels=96, learn_filters=False, pad_mode='circular', default_update_ratio=0.5):
         super().__init__()
         # not-learnable
         self.chn = channels
+        self._default_update_ratio = default_update_ratio
         # learnable layers
         if learn_filters:
-            self.filter = ChannelConv(list(torch.randn(4, 3, 3)), requires_grad=True)  # not in original
+            self.filter = ChannelConv(list(torch.randn(4, 3, 3)), pad_mode=pad_mode, requires_grad=True)  # not in original
         else:
-            self.filter = PresetFilters(requires_grad=False)
+            self.filter = PresetFilters(pad_mode=pad_mode, requires_grad=False)
         # learnable secondary layers
         self.w1 = torch.nn.Conv2d(in_channels=channels * 4, out_channels=hidden_channels, kernel_size=1)
         self.w2 = torch.nn.Conv2d(in_channels=hidden_channels, out_channels=channels, kernel_size=1, bias=False)
         self.w2.weight.data.zero_()
 
-    def _forward_single_iter(self, x, update_ratio=0.5):
+    def _forward_single_iter(self, x, update_ratio=None):
+        if update_ratio is None:
+            update_ratio = self._default_update_ratio
         # generate random update mask
         B, C, H, W = x.shape
         update_mask = torch.rand(B, 1, H, W, device=x.device) < update_ratio
@@ -69,11 +73,14 @@ class TextureNcaSystem(BaseSystemNCA):
     def __init__(
         self,
         style_img_url='https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Tempera%2C_charcoal_and_gouache_mountain_painting_by_Nicholas_Roerich.jpg/301px-Tempera%2C_charcoal_and_gouache_mountain_painting_by_Nicholas_Roerich.jpg',
-        img_size=128,
+        style_img_size=128,
         # nca options
+        nca_img_size=128,
         nca_learn_filters=False,
         nca_channels=12,
         nca_hidden_channels=96,
+        nca_pad_mode='circular',
+        nca_default_update_ratio=0.5,
         # training options
         iters=(32, 64),    # original is (64, 96)
         batch_size=4,      # original is 4
@@ -82,18 +89,18 @@ class TextureNcaSystem(BaseSystemNCA):
         pool_reset_element_period=2,
         pool_on_cpu=True,  # save GPU memory, especially if the pool is large
         # extra, not configurable in original implementation
-        normalize_gradient=True,  # try this off
-        consistency_loss=False,   # try this on
-        scale_loss=None,          # max loss value if you have stability problems
+        normalize_gradient=True,      # try this off
+        consistency_loss_scale=None,  # try this on
+        scale_loss=None,              # max loss value if you have stability problems
     ):
         super().__init__()
         self.save_hyperparameters()
         # check hparams
         self.hparams.iters_min, self.hparams.iters_max = (self.hparams.iters, self.hparams.iters) if isinstance(self.hparams.iters, int) else self.hparams.iters
         # create the model
-        self._nca = TextureNCA(channels=self.hparams.nca_channels, hidden_channels=self.hparams.nca_hidden_channels, learn_filters=self.hparams.nca_learn_filters)
+        self._nca = TextureNCA(channels=self.hparams.nca_channels, hidden_channels=self.hparams.nca_hidden_channels, learn_filters=self.hparams.nca_learn_filters, pad_mode=self.hparams.nca_pad_mode, default_update_ratio=self.hparams.nca_default_update_ratio)
         # training attributes
-        self.style_img = im_read(self.hparams.style_img_url, size=self.hparams.img_size)
+        self.style_img = im_read(self.hparams.style_img_url, size=self.hparams.style_img_size)
         self._style_loss = None
         self._organism_pool = None
 
@@ -108,7 +115,7 @@ class TextureNcaSystem(BaseSystemNCA):
     def on_train_start(self):
         # make loss & organism pool
         loss = StyleLoss(target_img=self.style_img)
-        pool = self.nca.make_start_batch(batch_size=self.hparams.pool_size, size=self.hparams.img_size)
+        pool = self.nca.make_start_batch(batch_size=self.hparams.pool_size, size=self.hparams.nca_img_size)
         # load to correct device, store pool on CPU to save some memory
         # style loss is too slow on CPU, but uses a fair bit of RAM
         self._style_loss = loss.to(self.device)
@@ -128,7 +135,7 @@ class TextureNcaSystem(BaseSystemNCA):
         with torch.no_grad():
             # reset runs in the pool
             if i % self.hparams.pool_reset_element_period == 0:
-                self._organism_pool[idxs[0]] = self.nca.make_start_batch(batch_size=1, size=self.hparams.img_size, device=self._organism_pool.device)[0]
+                self._organism_pool[idxs[0]] = self.nca.make_start_batch(batch_size=1, size=self.hparams.nca_img_size, device=self._organism_pool.device)[0]
             # load incomplete runs from the pool
             x = self._organism_pool[idxs].to(self.device)
             x_img = self.nca.extract_rgb(x)
@@ -138,10 +145,13 @@ class TextureNcaSystem(BaseSystemNCA):
         y, y_img = self.nca.forward(x, return_img=True, iterations=np.random.randint(self.hparams.iters_min, self.hparams.iters_max + 1))
         # compute cellular loss
         loss = self._style_loss(y_img)
+        self.log('style', loss, prog_bar=True)
 
         # organisms should minimize drift
-        if self.hparams.consistency_loss:
-            loss += F.mse_loss(y_img, x_img)
+        if self.hparams.consistency_loss_scale is not None:
+            consistency_loss = F.mse_loss(y_img, x_img) * self.hparams.consistency_loss_scale
+            self.log('consist', consistency_loss, prog_bar=True)
+            loss += consistency_loss
 
         # make sure the loss is not too large, especially prominent if we don't normalise the gradient
         if self.hparams.scale_loss is not None:
@@ -198,30 +208,43 @@ class TextureNcaSystem(BaseSystemNCA):
 
 if __name__ == '__main__':
 
-    out_dir = f'out/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+    class FireRunner(object):
+        def __init__(
+            self,
+            train_steps: int = 5000,
+            train_cuda: bool = torch.cuda.is_available(),
+            # visualise
+            vis_period_plt: int = 500,
+            vis_period_vid: int = 2500,
+            vis_im_size: int = 256,
+            vis_out_dir: str = f'out/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}',
+            show: bool = False,
+        ):
+            self._show = show
+            self._trainer = pl.Trainer(
+                max_steps=train_steps,
+                checkpoint_callback=False,
+                logger=False,
+                callbacks=[
+                    CallbackImshowNCA(period=vis_period_plt, start_batch_kwargs=dict(size=vis_im_size), save_dir=vis_out_dir, show=self._show),
+                    CallbackVidsaveNCA(period=vis_period_vid, start_batch_kwargs=dict(size=vis_im_size), save_dir=vis_out_dir)
+                ],
+                gpus=1 if train_cuda else 0,
+            )
 
-    trainer = pl.Trainer(
-        max_steps=10000,
-        checkpoint_callback=False,
-        logger=False,
-        callbacks=[
-            CallbackImshowNCA(period=500, start_batch_kwargs=dict(size=256), save_dir=out_dir),
-            CallbackVidsaveNCA(period=1000, start_batch_kwargs=dict(size=256), save_dir=out_dir)
-        ],
-        gpus=1,
-    )
+        @wraps(TextureNcaSystem.__init__)
+        def run(self, **kwargs):
+            # initialise system & train
+            system = TextureNcaSystem(**kwargs)
+            im_show(system.style_img, title='target_img', show=self._show)
+            self._trainer.fit(system)
 
-    # default settings use about 5614MiB of RAM
-    system = TextureNcaSystem(
-        style_img_url='microbe.png',
-        # extras
-        consistency_loss=True,    # not enabled in original version
-        normalize_gradient=False, # enabled in original version
-    )
-
-    im_show(system.style_img, title='target_img')
-
-    trainer.fit(system)
+    # entry point
+    # eg. $ example_texture_nca.py --help
+    # eg. $ example_texture_nca.py run --help
+    # eg. $ example_texture_nca.py --train_steps=500 run --lr=0.0005
+    import fire
+    fire.Fire(FireRunner, command='run', name='runner')
 
 
 # ========================================================================= #
